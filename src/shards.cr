cbed1b750bd20329c549dd10d87c83cb1b433f41
Paul Smith <paulcsmith0218@gmail.com>
2020-07-16 13:07:21 -0400

    WIP Pulsar

--- src/shards.cr
+++ src/shards.cr
@@ -2,6 +2,41 @@
 require "dotenv"
 Dotenv.load?
 
+abstract class Pulsar::Event
+  getter :created_at
+
+  macro inherited
+    @created_at = Time.utc
+    class_property subscribers = [] of Proc(self, Nil)
+  end
+
+  def self.subscribe(proc)
+    self.subscribers << proc
+  end
+
+  def publish
+    self.class.subscribers.each do |s|
+      s.call(self)
+    end
+  end
+
+  abstract def name : String
+end
+
+class AvramQueryEvent < Pulsar::Event
+  property :query
+
+  def initialize(@query : String)
+  end
+
+  def name : String
+    "avram.query"
+  end
+end
+
+AvramQueryEvent.subscribe ->(event : AvramQueryEvent) { puts event.inspect }
+AvramQueryEvent.new(query: "foo").publish
+
 # Require your shards here
 require "avram"
 require "lucky"
