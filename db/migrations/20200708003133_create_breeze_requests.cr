cfe7386df3d54aaf40ee0cd321641d1fbca7da3a
Paul Smith <paulcsmith0218@gmail.com>
2020-07-27 13:59:49 -0400

    Put it all in one migration

--- db/migrations/20200708003133_create_breeze_requests.cr
+++ db/migrations/20200708003133_create_breeze_requests.cr
@@ -1,4 +1,4 @@
-class CreateBreezeRequests::V20200327160456 < Avram::Migrator::Migration::V1
+class CreateBreezeRequests::V2 < Avram::Migrator::Migration::V1
   def migrate
     # Learn about migrations at: https://luckyframework.org/guides/database/migrations
     create table_for(BreezeRequest) do
@@ -12,9 +12,18 @@ class CreateBreezeRequests::V20200327160456 < Avram::Migrator::Migration::V1
       add headers : JSON::Any
       add session : JSON::Any
     end
+
+    create table_for(BreezeSqlStatement) do
+      primary_key id : Int64
+      add_timestamps
+      add statement : String
+      add args : String?
+      add_belongs_to breeze_request : BreezeRequest?, on_delete: :nullify
+    end
   end
 
   def rollback
     drop table_for(BreezeRequest)
+    drop table_for(BreezeSqlStatement)
   end
 end
