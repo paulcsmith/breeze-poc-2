28832e40e4b237d9dd3c88e97c02d9e4b5cace05
Paul Smith <paulcsmith0218@gmail.com>
2020-07-27 16:50:43 -0400

    Use divide utility classes

--- src/pages/breeze/queries/index_page.cr
+++ src/pages/breeze/queries/index_page.cr
@@ -12,7 +12,7 @@ class Breeze::Queries::IndexPage < BreezeLayout
 
   def render_table
     m Breeze::Panel do
-      ul class: "-mt-px" do
+      ul class: "mt-1 divide-y divide-gray-200" do
         @breeze_queries.each do |query|
           query_row(query)
         end
@@ -23,7 +23,7 @@ class Breeze::Queries::IndexPage < BreezeLayout
   end
 
   def query_row(query)
-    li class: "border-t border-gray-200" do
+    li do
       link Queries::Show.with(query.id), class: "block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out" do
         div class: "flex items-center px-4 py-4 sm:px-4" do
           div class: "min-w-0 flex-1 flex items-center" do
--- src/pages/breeze/requests/index_page.cr
+++ src/pages/breeze/requests/index_page.cr
@@ -12,7 +12,7 @@ class Breeze::Requests::IndexPage < BreezeLayout
 
   def render_table
     m Breeze::Panel do
-      ul class: "-mt-px" do
+      ul class: "mt-1 divide-y divide-gray-200" do
         @breeze_requests.each do |req|
           request_row(req)
         end
@@ -23,7 +23,7 @@ class Breeze::Requests::IndexPage < BreezeLayout
   end
 
   def request_row(req)
-    li class: "border-t border-gray-200" do
+    li do
       link class: "block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out", to: Show.with(req.id) do
         div class: "flex items-center px-4 py-4 sm:px-4" do
           div class: "min-w-0 flex-1 flex items-center" do
