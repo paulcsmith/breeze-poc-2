cab84d3f01a63cae704933b62a4f81183b507707
Paul Smith <paulcsmith0218@gmail.com>
2021-02-23 12:31:27 -0500

    Had to add this for some reason

--- src/actions/breeze_action.cr
+++ src/actions/breeze_action.cr
@@ -1,6 +1,7 @@
 require "./browser_action"
 
 abstract class BreezeAction < Lucky::Action
+  include Lucky::ProtectFromForgery
   include Lucky::Paginator::BackendHelpers
   accepted_formats [:html]
 end
